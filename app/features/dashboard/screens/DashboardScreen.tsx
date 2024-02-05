62db65c6b297297c9a701797bb610f2af9a92691
arturvoloshyn <arturvoloshyn@gmail.com>
2024-02-05 22:53:32 +0200

    fix(dashboard/screens): fix types

--- app/features/dashboard/screens/DashboardScreen.tsx
+++ app/features/dashboard/screens/DashboardScreen.tsx
@@ -1,9 +1,12 @@
 import { Alert } from "react-native";
 import { DashboardScreenView } from "./DashboardScreenView";
 import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+type NavigationProp = NativeStackNavigationProp<{ Profile: undefined }>;
 
 export const DashboardScreen = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<NavigationProp>();
   return (
     <DashboardScreenView
       onShowProfile={() => navigation.navigate("Profile")}
